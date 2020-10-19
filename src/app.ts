54c45fd08bf14159fbf59605ef0f2b29b6356eb5
Eugene <eugzubv@gmail.com>
2020-10-19 22:34:39 +0300

    start server on db connection

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,16 @@ import express from "express";
 import Database from "./db";
 
 const app: express.Application = express();
+const port = 3000 || process.env.PORT;
 
 Database.authenticate()
   .then(() => {
     console.log("Connected to database successfully.");
+
+    app.listen(port, function () {
+      console.log(`Server start to listen on ${port} port.`);
+    });
   })
   .catch(err => {
     console.error(`Unable to connect to database: ${err}`);
   });
-
-app.listen(3000 || process.env.PORT);
